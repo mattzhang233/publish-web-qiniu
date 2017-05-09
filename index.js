4f63eeba4fdea65798ff5a9146640e0935a93b37
yujingwyh <yujingwyh@163.com>
2017-05-09 18:11:04 +0800

    更新

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-var qiniu = require('qiniu');
+
 var argv = require('yargs').argv;
 
 var config = require('./lib/config')();
 var log = require('./lib/log')
+var upload = require('./lib/upload')
 
 //读取设置
-config.then(function (data) {
-  console.error(data)
-},function (err) {
-  log(err,'error');
-}).then(function () {
-  
-},function () {
-  
-});
+config
+  .then(function (data) {
+    return upload(data);
+  }, handleError)
+  .then(function () {
+
+  }, handleError);
+
+
+function handleError(message) {
+  log(message, 'error');
+}
