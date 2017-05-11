2bc197ecd9b40f93b7e7855aa77e5d69e69c8a19
yujingwyh <yujingwyh@163.com>
2017-05-11 17:55:31 +0800

    更新

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-var argv = require('yargs').argv;
+var isObject = require('is-object');
+var isArray = require('is-array');
+var isString = require('is-string');
+var vfs = require('vinyl-fs');
+var mapStream = require('map-stream');
 
-var config = require('./lib/config')
-var log = require('./lib/log')
-var upload = require('./lib/upload')
-var replace = require('./lib/replace')
+var unit = require('./lib/unit');
+var defaultConfig = require('./lib/config');
 
-config()
-  .then(function (data) {
-    return upload(data);
-  }, handleError)
-  .then(function (config,files) {
-    return replace(config,files);
-  }, handleError).then(function () {
-  console.log('finish')
-},handleError);
+function handleSrc(src) {
+  var srcs = [];
 
+  if(isObject(src)){
+    for(var key in src){
+      if(src.hasOwnProperty(key)){
+        srcs = srcs.concat(handleSrc(src[key]));
+      }
+    }
+  }
+  else if(isArray(src)){
+    srcs = src;
+  }
+  else if(isString(src)){
+    srcs = [src];
+  }
+  else{
+    unit.log('config.uploadSrc 不是一个合法的参数','error');
+    process.exit(0);
+  }
 
-function handleError(message) {
-  log(message, 'error');
+  return srcs;
 }
+
+function plugin(config) {
+  var config = Object.assign(defaultConfig,config);
+  var srcs = handleSrc(config.uploadSrc);
+
+  vfs.src(srcs)
+    .pipe(mapStream(function (file,cb) {
+      console.log(file)
+    }))
+}
+plugin();
+module.exports = plugin;
