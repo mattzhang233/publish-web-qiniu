3ee913ef72d6df9c0188c657ca8889d608c70011
yujingwyh <yujingwyh@163.com>
2017-05-19 18:32:07 +0800

    更新

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # publish-web-qiniu
 
-上传web资源到七牛云并替换路径
+通过正则匹配需要上传的文件后上传到七牛云并替换路径
 
 ## 安装
 
@@ -20,7 +20,7 @@ publishWeb({
   qiniuAccess: '七牛云的access',
   qiniuSecret: '七牛云的secret',
   qiniuBucket: '上传到的bucket',
-  qiniuBucketDomain:'bucket域名'
+  qiniuBucketDomain:'bucket对应域名'
 });
 ```
 
@@ -50,6 +50,27 @@ Type: `regexp`<br>
 Default: `/(?:href=|src=|url\()['|"]([^\s>"']+?)['|"]/gi`
 
 上传查找规则
+
+##### uploadedDelete
+
+Type: `boolean`<br>
+Default: `false`
+
+上传完成后是否需要删除该文件
+
+##### onCollected
+
+Type: `function`<br>
+Default: `null`
+
+在收集完成后回调，传入参数为pwqfiles，返回pwqfiles或者promise（resolve的数据为pwqfiles）
+
+##### onFinish
+
+Type: `function`<br>
+Default: `null`
+
+在处理完成时回调，可以返回promise
 
 ## License
 
--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-var unit = require('./libs/unit');
-var conf = require('./libs/config');
-var collect = require('./libs/collect')
-var upload = require('./libs/upload')
-var replace = require('./libs/replace')
-var Pwqfiles = require('./libs/pwqfiles')
+var unit = require('./lib/unit');
+var conf = require('./lib/config');
+var collect = require('./lib/collect');
+var upload = require('./lib/upload');
+var replace = require('./lib/replace');
+var Pwqfiles = require('./lib/pwqfiles');
+var ora = require('ora');
 
-function handleError(errorMessage) {
+var spinner;
 
-  unit.log(errorMessage, 'error');
-}
+
 function plugin(config) {
   var pwqfiles;
 
+  spinner = ora('init configing').start();
+
   conf(config).then(function (data) {
     config = data;
+
+    spinner.stop();
+    unit.log('init config finish', 'info');
+    spinner = ora('collecting need to upload the file').start();
 
     return collect(config);
   }).then(function (files) {
@@ -21,13 +27,64 @@ function plugin(config) {
 
     return pwqfiles.init(config);
   }).then(function () {
-    return upload.filterAndRecord(config, pwqfiles);
+    return new Promise(function (resolve, reject) {
+      var cbData;
+
+      if (config.onCollected) {
+        cbData = config.onCollected(pwqfiles);
+
+        if (cbData && cbData.then) {
+          cbData.then(function (argPwqfiles) {
+            pwqfiles = argPwqfiles;
+
+            upload.filterAndRecord(config, pwqfiles).then(resolve, reject);
+          }, reject);
+          return;
+        }
+        pwqfiles = cbData;
+      }
+      upload.filterAndRecord(config, pwqfiles).then(resolve, reject);
+    });
   }).then(function () {
+    spinner.stop();
+    unit.log('collect finish');
+    spinner = ora('replacing the file path').start();
+
     return replace(config, pwqfiles);
   }).then(function (data) {
-    return upload.uploadToQiniu(config)
-  }).then(function () {
-    console.log('完成');
+    spinner.stop();
+    unit.log('replace finish');
+    spinner = ora('uploading files').start();
+
+    return upload.uploadToQiniu(config, spinner)
+  }).then(function (total) {
+    var cbData;
+    if (config.onFinish) {
+      cbData = config.onFinish();
+
+      if (cbData && cbData.then) {
+        cbData.then(function () {
+          handleFinish(total);
+        }, handleError);
+        return;
+      }
+    }
+    handleFinish(total);
   }, handleError);
+
+  function handleFinish(total) {
+    spinner.stop();
+    unit.log('upload finish');
+    unit.log('replaced ' + pwqfiles.replaceFiles.length + ' files,uploaded ' + total + ' files', 'warn');
+    unit.log('publish finish');
+  }
+
+  function handleError(errorMessage) {
+    if (spinner && spinner.stop) {
+      spinner.stop();
+    }
+
+    unit.log(errorMessage, 'error');
+  }
 }
 module.exports = plugin;
